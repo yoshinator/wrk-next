0e7b741ab888ea565109404e6830c58a3a6628c0
Yoan <yoanante@gmail.com>
2024-02-25 21:59:52 -0500

    Refactor ResponsiveDrawer to have working links and move constants out.

--- src/app/components/ResponsiveDrawer/ResponsiveDrawer.tsx
+++ src/app/components/ResponsiveDrawer/ResponsiveDrawer.tsx
@@ -1,31 +1,33 @@
 'use client'
-import * as React from 'react'
+import { Fragment, useState } from 'react'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import CssBaseline from '@mui/material/CssBaseline'
 import Divider from '@mui/material/Divider'
 import Drawer from '@mui/material/Drawer'
 import IconButton from '@mui/material/IconButton'
-import InboxIcon from '@mui/icons-material/MoveToInbox'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
-import MailIcon from '@mui/icons-material/Mail'
 import MenuIcon from '@mui/icons-material/Menu'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 
-const drawerWidth = 240
+import { drawerWidth, listItems } from './constants'
 
 interface Props {
   children: React.ReactNode
 }
 
 const ResponsiveDrawer: React.FC<Props> = ({ children }) => {
-  const [mobileOpen, setMobileOpen] = React.useState(false)
-  const [isClosing, setIsClosing] = React.useState(false)
+  const [mobileOpen, setMobileOpen] = useState(false)
+  const [isClosing, setIsClosing] = useState(false)
+  const pathName = usePathname()
 
   const handleDrawerClose = () => {
     setIsClosing(true)
@@ -45,100 +47,100 @@ const ResponsiveDrawer: React.FC<Props> = ({ children }) => {
   const drawer = (
     <div>
       <Toolbar />
-      <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <Box>
+        <List>
+          {listItems.map((item) => (
+            <Fragment key={item.text}>
+              <Link
+                href={item.link}
+                style={{ color: 'inherit', textDecoration: 'none' }}
+              >
+                <ListItem disablePadding>
+                  <ListItemButton
+                    component="span"
+                    selected={pathName === item.link}
+                  >
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.text} />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+              {item.divider && <Divider />}
+            </Fragment>
+          ))}
+        </List>
+      </Box>
     </div>
   )
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+          }}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Responsive drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        >
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onTransitionEnd={handleDrawerTransitionEnd}
+            onClose={handleDrawerClose}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': {
+                boxSizing: 'border-box',
+                width: drawerWidth,
+              },
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onTransitionEnd={handleDrawerTransitionEnd}
-          onClose={handleDrawerClose}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
-              width: drawerWidth,
-            },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
+            {drawer}
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: 'none', sm: 'block' },
+              '& .MuiDrawer-paper': {
+                boxSizing: 'border-box',
+                width: drawerWidth,
+              },
+            }}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box mt={8} mx={1} px={1} width="100%" minHeight="calc(100vh - 64px)">
+          {children}
+        </Box>
       </Box>
-      {children}
-    </Box>
+    </>
   )
 }
 
---
+++ src/app/components/ResponsiveDrawer/constants.tsx
@@ -0,0 +1,22 @@
+import ContactsIcon from '@mui/icons-material/Contacts'
+import CopyAll from '@mui/icons-material/CopyAll'
+import MailIcon from '@mui/icons-material/Mail'
+import EventRepeatIcon from '@mui/icons-material/EventRepeat'
+import AccountBoxIcon from '@mui/icons-material/AccountBox'
+import SettingsIcon from '@mui/icons-material/Settings'
+
+export const drawerWidth = 240
+
+export const listItems = [
+  { text: 'Contacts', icon: <ContactsIcon />, link: '/dashboard/contacts' },
+  { text: 'Templates', icon: <CopyAll />, link: '/dashboard/templates' },
+  { text: 'Lists', icon: <MailIcon />, link: '/dashboard/lists' },
+  {
+    text: 'Auto Contact',
+    icon: <EventRepeatIcon />,
+    link: '/dashboard/auto-contact',
+    divider: true,
+  },
+  { text: 'Profile', icon: <AccountBoxIcon />, link: '/dashboard/profile' },
+  { text: 'Settings', icon: <SettingsIcon />, link: '/dashboard/settings' },
+]
